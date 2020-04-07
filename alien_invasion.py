c9c8845eb684152e0d6c5963842cb85dd3f778cb
Brian Taylor Johnson <briantaylorjohnson@gmail.com>
2020-04-07 00:29:04 -0400

    Created new Button() class; rendered Play button if game is inactive to start game

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -41,6 +42,9 @@ class AlienInvasion:
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+
+        # Create the Play button
+        self.play_button = Button(self, 'Play')
 
     def run_game(self):
 
@@ -243,6 +247,10 @@ class AlienInvasion:
 
         # Render aliens
         self.aliens.draw(self.screen)
+
+        # Render button if game is inactive
+        if not self.stats.game_active:
+            self.play_button.draw_button()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
