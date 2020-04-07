634f403ee927590ed18d5c5cb0fa7e8148b3be8c
Brian Taylor Johnson <briantaylorjohnson@gmail.com>
2020-04-07 00:38:04 -0400

    Added code to reset the game stats when a new game starts; deactivites the button area if the game is active

--- alien_invasion.py
+++ alien_invasion.py
@@ -116,8 +116,20 @@ class AlienInvasion:
     def _check_play_button(self, mouse_pos):
 
         """ Start a new game when the play clicks Play. """
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+
+            # Reset the game statistics
+            self.stats.reset_stats()
             self.stats.game_active = True
+
+            # Get rid of the any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
 
     def _fire_bullet(self):
 
