48cf38d3f80388178b2e316e22971a7859c6c229
Brian Taylor Johnson <briantaylorjohnson@gmail.com>
2020-04-07 01:13:56 -0400

    Coded logic to increase score with bullet to alien hits; also resets score when a new game starts

--- alien_invasion.py
+++ alien_invasion.py
@@ -127,6 +127,7 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             # Get rid of the any remaining aliens and bullets.
             self.aliens.empty()
@@ -164,6 +165,10 @@ class AlienInvasion:
         """ Respond to bullet-alien collisions. """
         # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if collisions:
+            self.stats.score += self.settings.alien_points
+            self.sb.prep_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
