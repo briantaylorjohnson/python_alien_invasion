a086f7e0e51db6692f6f1a717389da7341e9b06e
Brian Taylor Johnson <briantaylorjohnson@gmail.com>
2020-04-06 23:55:05 -0400

    Enabled tracking of alien-ship collisions

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings:
         self.ship_speed = 15
 
         # Bullet Settings
-        self.bullet_speed = 15
+        self.bullet_speed = 25
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
@@ -23,6 +23,6 @@ class Settings:
 
         # Alien Settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 100
         """ fleet_direction of 1 represents right; -1 represents left. """ 
         self.fleet_direction = 1
