1042be02c4060f0562ee926ea4ad6d3adf57cb7a
Brian Taylor Johnson <briantaylorjohnson@gmail.com>
2020-04-08 00:25:47 -0400

    Updated game instance to open in 1200x800 window instead of full screen; this will make the game render better across differing screen sizes

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,9 +23,9 @@ class AlienInvasion:
         self.settings = Settings()
 
         # Set the game to open in full screen mode
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
+        self.screen = pygame.display.set_mode((1200,800))
+        self.settings.screen_width = 1200
+        self.settings.screen_height = 800
 
         # Set the title for the game
         pygame.display.set_caption('Alien Invasion')
